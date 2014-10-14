563788750b380d6340bac6394e2f995a16ef7093
velialarm <velialarm@gmail.com>
2014-10-14 20:44:19 +0300

    update  Android Mobile Applications

--- Mobile Development/Android Mobile Applications/06-Getting Started with Android/exersise/TwoActivity/src/com/example/twoactivity/SecondActivity.java
+++ Mobile Development/Android Mobile Applications/06-Getting Started with Android/exersise/TwoActivity/src/com/example/twoactivity/SecondActivity.java
@@ -1,12 +1,14 @@
 package com.example.twoactivity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class SecondActivity extends ActionBarActivity {
 
+	//private Context context;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
