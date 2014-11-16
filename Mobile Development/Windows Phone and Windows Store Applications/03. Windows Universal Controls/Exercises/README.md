a86df11afd00674bcddb26a4458f12607b63c8a6
velialarm <velialarm@gmail.com>
2014-11-16 20:41:36 +0200

    Add exercise for Windows Universal Controls

---
+++ Mobile Development/Windows Phone and Windows Store Applications/03. Windows Universal Controls/Exercises/README.md
@@ -0,0 +1,11 @@
+<p align="center"><a href="http://academy.telerik.com/">
+<img src="https://raw.githubusercontent.com/velialarm/TelerikAcademy/master/telerik-academy.png" /></a></p>
+
+# Windows Phone and Windows Store Applications
+
+	1. Write a program that show the simple window with one TextBox. Add text to the TextBox.If you select some text in the TextBox–display the current selection information.
+	2. Write a program with a Buttonand a TextBlock. The TextBlockshould show the number of clicks on the button.
+	3. Write a program that shows a ComboBoxwith various elements added to its Items. For example –add text, ellipse and picture.
+
+
+
---
+++ Mobile Development/Windows Phone and Windows Store Applications/03. Windows Universal Controls/Exercises/UnivCont/02.NumberOfClick/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace _02.NumberOfClick
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void IncreaseNumber(object sender, RoutedEventArgs e)
+        {
+            this.resultOfClick.Text = (int.Parse(this.resultOfClick.Text) + 1).ToString();
+        }
+    }
+}
